619943188b77eeee9d0a08b9a1e4f9c87b457da1
bogaclau <sebba30015boga@gmail.com>
2023-08-21 19:27:46 -0300

    tipos primitivos

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
-let miNuevoLorem = "Lorem ipsum";
+// tipos datos
 
-console.log(miNuevoLorem);
+let myString = "hola mundo";
+let myString2 = "hola mundo"; // puede ser con comillas simples
+let myString3 = `
+hola mundo
+asdasd
+asdasd
+asdasda
+sda
+sdasda
+sdas
+`;
+
+console.log(typeof myString);
+
+let myNumber = 127263489576238947562938746587;
+let myBigInt = 127263489576238947562938746587n;
+
+console.log(typeof myNumber);
+console.log(typeof myBigInt);
+
+let myUndefined = undefined;
+
+console.log(typeof myUndefined);
+
+let myNull = null;
+
+console.log("null");
+
+let mySymbol = Symbol();
+
+console.log(typeof mySymbol);
+
+let myBoolean = false;
+
+console.log(typeof myBoolean);
+
+myBoolean = true;
+
+console.log(typeof myBoolean);
