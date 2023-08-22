7fe4d5859e6e91602cb2dc2a0ce2a8541d9441d7
bogaclau <sebba30015boga@gmail.com>
2023-08-22 17:58:17 -0300

    Condicionales

--- main.js
+++ main.js
@@ -1,84 +1,36 @@
-// arrays
+let miVariable = 5;
 
-const alumnosDeLaComision = [
-  "Lucas Arenas",
-  "Pablo Mariasch",
-  "Federico Tortolero",
-  "Matias Ortiz",
-  "Alfredo Oscar",
-  "Hugo Giovanetti",
-  12,
-  53,
-  32,
-  21,
-  23,
-  true,
-  false,
-  Symbol(),
-  undefined,
-  null,
-];
-
-console.log(alumnosDeLaComision[16]);
-
-alumnosDeLaComision.push("Alfredo Oscar");
-
-console.log(alumnosDeLaComision[16]);
-
-alumnosDeLaComision.pop();
-
-console.log(alumnosDeLaComision[16]);
-
-// objects
-
-let alumno = {
-  nombre: "Dario",
-  apellido: "Legue",
-  edad: 25,
-  casado: true,
-  "fecha-de-nacimiento": "17/09/1990",
-};
-
-function restarIVA(salario) {
-  salario = salario * 0.79;
-  return salario;
+if (miVariable == 3) {
+  console.log("Entre en el if");
+} else if (miVariable == 4) {
+  console.log("Entre en el else if");
+} else {
+  console.log("Salida por defecto");
 }
 
-let miSueldo = restarIVA(100);
-let miSueldo2 = restarIVA(1000);
-
-console.log(miSueldo);
-console.log(miSueldo2);
-
-var miVariable = "chau";
-
-function miFuncion() {
-  var miVariable = "hola";
-}
-
-console.log(miVariable);
-
-miFuncion();
-
-console.log(miVariable);
-
-function varTest() {
-  var x = 31;
-  if (true) {
-    var x = 71; // ¡misma variable!
-    console.log(x); // 71
+switch (miVariable) {
+  case 3: {
+    console.log("mi variable es igual a 3");
+    break;
   }
-  console.log(x); // 71
-}
-
-function letTest() {
-  let x = 31;
-  if (true) {
-    let x = 71; // variable diferente
-    console.log(x); // 71
+  case 4: {
+    console.log("mi variable es igual a 4");
+    break;
   }
-  console.log(x); // 31
+  case 5: {
+    console.log("mi variable es igual a 5");
+    break;
+  }
+  case 6: {
+    console.log("mi variable es igual a 6");
+    break;
+  }
+  case 7: {
+    console.log("mi variable es igual a 7");
+    break;
+  }
+  default: {
+    console.log("valor por defecto");
+    break;
+  }
 }
-// llamamos a las funciones
-varTest();
-letTest();
