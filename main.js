4f288836c7d140e10412c623ab58b628a908ee0f
bogaclau <sebba30015boga@gmail.com>
2023-08-22 18:30:23 -0300

    ternarias y template strings

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-let iterador = 1;
+5 == 5; // --> true o un false
 
-while (iterador <= 10) {
-  if (iterador != 5) {
-    console.log("while ", iterador);
-    iterador++;
-  } else {
-    iterador++;
-    return;
-  }
+let miVariable = 5 == 5 ? "Es true" : "Es false";
+
+let miVariable2;
+
+if (5 == 5) {
+  miVariable2 = "Es true";
+} else {
+  miVariable2 = "Es false";
 }
 
-let i = 1;
+console.log(miVariable);
 
-do {
-  console.log("do while", i);
-  i++;
-} while (false);
+let num1 = 5;
+let num2 = 10;
 
-for (let iter = 0; iter < 10; iter++) {
-  console.log("For ", iter);
-}
+let miString = `hola mundo ${num1 + num2}`;
+
+let miArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+console.table(miArray);
