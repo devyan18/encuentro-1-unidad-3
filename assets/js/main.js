8ea71a97c15c6fd2bb8e46dfa85dad3cfd356ad5
bogaclau <sebba30015boga@gmail.com>
2023-08-22 19:13:03 -0300

    modularizacion con import y export

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+import { multiplicacion, suma, division, resta } from "./operations.js";
+
+const mult = multiplicacion(2, 6);
+const sum = suma(2, 6);
+
+console.log(mult, sum);
---
+++ assets/js/operations.js
@@ -0,0 +1,15 @@
+export function suma(num1, num2) {
+  return num1 + num2;
+}
+
+export const resta = (num1, num2) => {
+  return num1 - num2;
+};
+
+export function multiplicacion(num1, num2) {
+  return num1 * num2;
+}
+
+export const division = (num1, num2) => {
+  return num1 / num2;
+};
