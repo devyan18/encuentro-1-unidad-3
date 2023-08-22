f6b39f22aded05ddf951fd7e0c6a8d39c1cc9aae
bogaclau <sebba30015boga@gmail.com>
2023-08-22 19:03:48 -0300

    functions, arrow functions, destructuring

--- main.js
+++ main.js
@@ -1,26 +1,60 @@
-5 == 5; // --> true o un false
+// funciones
 
-let miVariable = 5 == 5 ? "Es true" : "Es false";
-
-let miVariable2;
-
-if (5 == 5) {
-  miVariable2 = "Es true";
-} else {
-  miVariable2 = "Es false";
+function nombreDeLaFuncion(num1, num2) {
+  console.log("lorem 50 palabras");
+  console.log(num1 + num2);
+  console.log(true, false);
 }
 
-console.log(miVariable);
+// nombreDeLaFuncion(12, 53);
+// nombreDeLaFuncion(2, 3);
+// nombreDeLaFuncion("12", "32");
 
-let num1 = 5;
-let num2 = 10;
+function cuatroOperacionesBasicas(num1, num2) {
+  console.log(num1 + num2);
+  console.log(num1 - num2);
+  console.log(num1 * num2);
+  console.log(num1 / num2);
+}
 
-let miString = `hola mundo ${num1 + num2}`;
+//cuatroOperacionesBasicas(4, 0);
 
-let miArray = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+const suma = (num1 = 0, num2 = 0) => {
+  console.log(typeof num1);
+  console.log(typeof num2);
+  console.log(num1 + num2);
+};
 
-console.table(miArray);
+//suma();
+
+// parametros nombrados
+const division = ({ dividendo, divisor }) => {
+  console.log(dividendo / divisor);
+};
+
+division({
+  dividendo: 10,
+  divisor: 2,
+});
+
+// function resta(num1, num2) {
+//   return num1 - num2;
+// }
+
+// let valorDeLaResta = resta(1, 2);
+
+// console.log(valorDeLaResta);
+
+// desestructuracion
+
+const objetoCualquiera = {
+  nombre: "asdasd",
+  apellido: "asdasd",
+  edad: 23,
+};
+
+const { nombre, apellido, edad } = objetoCualquiera;
+
+const arrayCualquiera = [1, 2, "hola", "chau", true];
+
+const [primero, segundo, tercero, cuarto, quinto] = arrayCualquiera;
