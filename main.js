1c41dba6731a88064b099088eee9a5cbe099364f
bogaclau <sebba30015boga@gmail.com>
2023-08-22 18:20:08 -0300

    Bucles

--- main.js
+++ main.js
@@ -1,36 +1,22 @@
-let miVariable = 5;
+let iterador = 1;
 
-if (miVariable == 3) {
-  console.log("Entre en el if");
-} else if (miVariable == 4) {
-  console.log("Entre en el else if");
-} else {
-  console.log("Salida por defecto");
+while (iterador <= 10) {
+  if (iterador != 5) {
+    console.log("while ", iterador);
+    iterador++;
+  } else {
+    iterador++;
+    return;
+  }
 }
 
-switch (miVariable) {
-  case 3: {
-    console.log("mi variable es igual a 3");
-    break;
-  }
-  case 4: {
-    console.log("mi variable es igual a 4");
-    break;
-  }
-  case 5: {
-    console.log("mi variable es igual a 5");
-    break;
-  }
-  case 6: {
-    console.log("mi variable es igual a 6");
-    break;
-  }
-  case 7: {
-    console.log("mi variable es igual a 7");
-    break;
-  }
-  default: {
-    console.log("valor por defecto");
-    break;
-  }
+let i = 1;
+
+do {
+  console.log("do while", i);
+  i++;
+} while (false);
+
+for (let iter = 0; iter < 10; iter++) {
+  console.log("For ", iter);
 }
